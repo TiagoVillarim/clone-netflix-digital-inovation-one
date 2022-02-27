e96e02a765caccc346dd201cfc72df4d16579f19
Tiago villarim <tiagovillarim@gmail.com>
2022-02-27 18:01:05 -0300

    saved

--- netflix/src/components/pages/index.js
+++ netflix/src/components/pages/index.js
@@ -1,5 +1,9 @@
 import React from "react";
 import Style from "../styles/main.css";
+import { FaPlay, FaInfoCircle } from "react-icons/fa";
+
+
+
 
 export default function Home() {
     return(
@@ -21,6 +25,17 @@ export default function Home() {
                     <div className="container">
                         <h3 className="titulo">Os simpsons</h3>
                         <p className="description">Os Simpsons moram na cidade de Springfield, uma cidade americana como qualquer outra. Homer trabalha como inspetor de segurança em uma usina de energia nuclear, Marge tenta manter a paz em sua família, Bart é um travesso garoto de 10 anos, Lisa, de 8 anos, é a inteligente da família que adora tocar saxofone e é vegetariana e a pequena Maggie conquista todos enquanto não larga sua chupeta. E a série tem um rico, e muitas vezes estranho, universo de personagens que também habitam Springfield. Com roteiros inteligentes, humor subversivo e divertidamente genial, a série faz piadas de si mesma e de todo mundo que aparecer em seu caminho.</p>
+
+                    <div className="buttons">
+                            <button role="button" className="button">
+                                <i><FaPlay/></i>
+                                ASSISTIR AGORA
+                            </button>
+                            <button role="button" className="button">
+                                <i><FaInfoCircle/></i>
+                                MAIS INFORMAÇÔES
+                            </button>
+                    </div>
                     </div>
                 </div>
             </main>
